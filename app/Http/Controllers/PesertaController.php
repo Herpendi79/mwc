d5977f9eb922beb0bbacc45d73bc57e9a11f359c
Herpendi79 <herpendi@gmail.com>
2026-05-10 14:38:13 +0700

    Artikel Upload

--- app/Http/Controllers/PesertaController.php
+++ app/Http/Controllers/PesertaController.php
@@ -452,7 +452,7 @@ class PesertaController extends Controller
             $submission = PesertaConferences::findOrFail($id_pc);
 
             // Security Check
-            if ($submission->id !== Auth::user()->peserta->id) {
+            if ($submission->id != Auth::user()->peserta->id) {
                 return redirect()->back()->with('error', 'Unauthorized action.');
             }
 
