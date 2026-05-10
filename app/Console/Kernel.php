6dae68980a7f27c0c89a6f6de4b92bf55b3eef54
Herpendi79 <herpendi@gmail.com>
2026-05-11 01:20:19 +0700

    Queue Email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,9 +17,7 @@ class Kernel extends ConsoleKernel
     {
         Log::info('Schedule ran at: ' . now()); // ← Tambahkan baris ini
 
-        $schedule->command('queue:work --stop-when-empty --tries=3')
-            ->everyMinute()
-            ->withoutOverlapping();
+        $schedule->command('queue:work --queue=conference --stop-when-empty')->everyMinute();
     }
 
 
