1d1d6a4001fd2220cbf028ff16679b74e2841e50
Herpendi79 <herpendi@gmail.com>
2026-06-25 21:08:29 +0700

    Filter

--- app/Http/Controllers/ReviewerController.php
+++ app/Http/Controllers/ReviewerController.php
@@ -455,6 +455,8 @@ class ReviewerController extends Controller
             $query->where('nama_user', 'like', '%' . $request->search . '%');
         }
 
+        $query->where('created_at', '<', '2026-06-26 00:00:00');
+
         $data = $query->orderBy('nama_user', 'asc')->get();
 
         // 3. Mapping Data WA & Negara (Logika Fallback agar Anna & ADAKSI Tampil)
