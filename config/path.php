072bc2c57e69cdf88947877c387cde5d90f70549
Herpendi79 <herpendi@gmail.com>
2026-07-03 08:04:14 +0700

    path

--- config/path.php
+++ config/path.php
@@ -3,8 +3,8 @@
 return [
     // Misal struktur: /home/user/laravel_projek dan /home/user/public_html
     //path awal
-    'submissions' => base_path('assets/file/submissions/'), // local
-   // 'submissions' => base_path('../public_html/uploads/file/submissions/'), //hosting
+    //'submissions' => base_path('assets/file/submissions/'),
+    'submissions' => base_path('../public_html/uploads/file/submissions/'),
     'articles'    => base_path('../public_html/uploads/file/articles/'),
     'sertifikat'    => base_path('../public_html/uploads/file/sertifikat/'), //hosting
    // 'sertifikat'    => base_path('public/assets/file/sertifikat/'), // local
@@ -13,8 +13,8 @@ return [
 
     // Digunakan di BLADE untuk menampilkan/link file (URL)
     //url awal
-     'submissions_url'  => asset('assets/file/submissions/'), //local
-   // 'submissions_url'  => 'https://adaksi.org/uploads/file/submissions/', //hosting
+    // 'submissions_url'  => asset('assets/file/submissions/'), //local
+    'submissions_url'  => 'https://adaksi.org/uploads/file/submissions/',
      // 'qrcode_url'  => env('APP_URL', 'http://localhost') . '/assets/file/qrcode/', //local
      'qrcode_url' => 'https://adaksi.org/uploads/file/qrcode/', // URL untuk hosting
      
