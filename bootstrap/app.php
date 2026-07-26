6b05eeea8c0e63da9d0a0bc7c4d857b73cf87cd8
Herpendi79 <herpendi@gmail.com>
2026-07-26 16:29:44 +0700

    Public

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
@@ -18,4 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
+
+$app->usePublicPath(dirname(__DIR__, 2) . '/public_html');
+
+return $app;
