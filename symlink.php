e17179be1314804bbf43b4fdfb403393fe2bd15e
Herpendi79 <herpendi@gmail.com>
2026-07-26 13:22:03 +0700

    Symlink

--- symlink.php
+++ symlink.php
@@ -6,6 +6,3 @@ if (symlink($target, $link)) {
 } else {
     echo "Gagal.";
 }
-
-//Pada bagian “$target = $_SERVER[‘DOCUMENT_ROOT’].”/../laravel/storage“;” Silahkan sesuaikan path target storage folder yang digunakan.
-//Lalu, pada bagian “$link = $_SERVER[‘DOCUMENT_ROOT’].”/storage“;” . Silahkan sesuaikan ke path dimana folder storage akan di link kan.
